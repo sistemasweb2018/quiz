792cfbcfd2bcc92efa63f54f5f775c7397daa527
jipvazoj <vadillo@ehu.es>
2015-06-08 17:43:58 +0200

    En Heroku

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,9 @@
+exports.question=function(req,res){
+res .render('quizes/question',{pregunta: 'Capital de Euskadi'});
+};
+exports.answer=function(req,res){
+if (req.query.respuesta==='Vitoria'){
+	res .render('quizes/answer',{respuesta: 'Correcta'})}
+else
+{res .render('quizes/answer',{respuesta: 'Incorrecta'})}
+};
